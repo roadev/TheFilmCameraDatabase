e6dd1acfa49972b117eaa951f19a6c471f8e5e92
Juan Roa <contact@juanroa.me>
2022-11-22 10:43:59 -0600

    setting up native base and eslint airbnb rules for RN TS

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 /**
  * @format
  */
+import React from "react";
+import { AppRegistry } from "react-native";
+import { extendTheme, NativeBaseProvider } from "native-base";
+import MyApp from "./App";
+import { name as appName } from "./app.json";
+// 1. Import the extendTheme function
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+// 2. Extend the theme to include custom colors, fonts, etc
+const newColorTheme = {
+  brand: {
+    900: "#8287af",
+    800: "#7c83db",
+    700: "#b3bef6",
+  },
+};
+const theme = extendTheme({ colors: newColorTheme });
+// 3. Pass the `theme` prop to the `NativeBaseProvider`
+function App() {
+  return (
+    <NativeBaseProvider theme={theme}>
+      <MyApp />
+    </NativeBaseProvider>
+  );
+}
 
 AppRegistry.registerComponent(appName, () => App);
